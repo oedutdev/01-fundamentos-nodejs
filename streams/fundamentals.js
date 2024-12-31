3bfdab11ec935df03fe8aba0a491c20264cbc34c
Eduardo Tavares <edut.santosdev@gmail.com>
2024-12-31 19:49:59 -0300

    criando stream de leitura

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -11,3 +11,31 @@
 // 10mb/s -> 10.000
 
 // Readable Streams / Writable Streams
+
+// Streams ->
+
+// process.stdin
+//   .pipe(process.stdout)
+
+import { Readable } from 'node:stream'
+
+class OneToHundredStream extends Readable {
+  index = 1
+
+  _read() {
+    const i = this.index++
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+        
+        this.push(buf)
+      }
+    }, 1000);
+  }
+}
+
+new OneToHundredStream()
+  .pipe(process.stdout)
