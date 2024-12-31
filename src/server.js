3f47eb2230860c2c91bcf6642f9c6bdcf70a5e27
Eduardo Tavares <edut.santosdev@gmail.com>
2024-12-31 20:54:32 -0300

    criando middleware de json

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 // ESModules => import/export
 
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 /**
  * Criar usuários
@@ -35,21 +36,10 @@ const users = []
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
     return res
-      .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users))
   }
 
