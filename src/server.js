d474d65207f9605002f7206be23c6495cff8ad13
Eduardo Tavares <edut.santosdev@gmail.com>
2024-12-31 20:58:40 -0300

    criando banco de dados json

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ import { json } from './middlewares/json.js'
  * HTTP Status Code
  */
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
@@ -39,18 +39,21 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-      .end(JSON.stringify(users))
+    const users = database.select('users')
+
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
 
-    users.push({
+    const user = {
       id: 1,
       name,
       email,
-    })
+    }
+
+    database.insert('users', user)
 
     return res.writeHead(201).end()
   }
