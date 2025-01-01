0f85fd82001022455b62ad1ee3519ca44ce53bc1
Eduardo Tavares <edut.santosdev@gmail.com>
2024-12-31 21:02:45 -0300

    criando id unico e universal uuid

--- src/server.js
+++ src/server.js
@@ -48,7 +48,7 @@ const server = http.createServer(async (req, res) => {
     const { name, email } = req.body
 
     const user = {
-      id: 1,
+      id: randomUUID(),
       name,
       email,
     }
